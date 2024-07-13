1015c957a2daa6b45a15360ea49e1eb9a09d5922
Vanessa Zanco <vanessazancof@gmail.com>
2024-07-13 12:09:30 +0100

    sound

--- src/main/java/com/br/musica/sound/principal/Principal.java
+++ src/main/java/com/br/musica/sound/principal/Principal.java
@@ -4,6 +4,7 @@ import com.br.musica.sound.model.Artista;
 import com.br.musica.sound.model.Musica;
 import com.br.musica.sound.model.TipoArtista;
 import com.br.musica.sound.repository.ArtistaRepository;
+import com.br.musica.sound.service.ConsultaChatGPT;
 
 import java.util.List;
 import java.util.Optional;
@@ -63,14 +64,22 @@ public class Principal {
     }
 
     private void pesquisarDadosDoArtista() {
+        System.out.println("Pesquisar dados sobre qual artista?");
+        var nome = leitura.nextLine();
+        var resposta = ConsultaChatGPT.obterInformacao(nome);
+        System.out.println(resposta.trim());
     }
 
     private void buscarMusicasPorArtista() {
+        System.out.println("Buscar músicas de que artista?");
+        var nome = leitura.nextLine();
+        List<Musica>musicas = repositorio.buscaMusicasPorArtistas(nome);
+        musicas.forEach(System.out::println);
     }
 
     private void listarMusicas() {
         List<Artista> artistas = repositorio.findAll();
-        artistas.forEach(System.out::println);
+        artistas.forEach(artist -> artist.getMusicas().forEach(System.out::println));
     }
 
     private void cadastrarMusicas() {
--- src/main/java/com/br/musica/sound/service/ICnverteDados.java
+++
@@ -1,5 +0,0 @@
-package com.br.musica.sound.service;
-
-public interface ICnverteDados {
-        <T> T  obterDados(String json, Class<T> classe);
-}
